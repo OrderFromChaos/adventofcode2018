f58bd42a6ce33c83f8f0dc7a3d0ee5f137e3b06a
Syris Norelli <snore001@ucr.edu>
2018-12-17 17:45:14 -0800

    solution to first part of 12

--- 12/12.py
+++ 12/12.py
@@ -1,29 +1,51 @@
-raw = '''initial state: #..#.#..##......###...###
-...## => #
-..#.. => #
-.#... => #
-.#.#. => #
-.#.## => #
-.##.. => #
-.#### => #
-#.#.# => #
-#.### => #
-##.#. => #
+raw = '''initial state: ##.####..####...#.####..##.#..##..#####.##.#..#...#.###.###....####.###...##..#...##.#.#...##.##..
 ##.## => #
-###.. => #
+....# => .
+.#.#. => #
+..### => .
+##... => #
+##### => .
 ###.# => #
-####. => #
-'''
+.##.. => .
+..##. => .
+...## => #
+####. => .
+###.. => .
+.#### => #
+#...# => #
+..... => .
+..#.. => .
+#..## => .
+#.#.# => #
+.#.## => #
+.###. => .
+##..# => .
+.#... => #
+.#..# => #
+...#. => .
+#.#.. => .
+#.... => .
+##.#. => .
+#.### => .
+.##.# => .
+#..#. => #
+..#.# => .
+#.##. => #'''
 
 # Data parsing
 raw = raw.split('\n')
 
-growbed = raw[0].split(' ')[2]
-rules = {x.split(' => ')[0]:'#' for x in raw[1:]}
-
+growbed = list(raw[0].split(' ')[2])
+ruleraw = [x.split(' => ') for x in raw[1:]]
+rules = {"".join(x[0]):"".join(x[1]) for x in ruleraw}
+# Jank time: assume stuff mostly goes right and not much left. This is from getting expectations from r/adventofcode
+growbed = list('...........') + growbed + list('...')
+prepend = len('...........')
 
 # Growing
-for generation in range(20):
+for generation in range(50000000000):
+    if generation % 1000000000 == 0:
+        print('billion!')
     # Extension condition
     end = len(growbed)-1
     for i in range(0,5):
@@ -35,5 +57,23 @@ for generation in range(20):
         growbed += ['.']*i
 
     # Actual growing
-    for plant in growbed:
-    
+    newbed = [x for x in growbed]
+    for index, plant in enumerate(growbed):
+        try:
+            lower = index-2
+            if lower < 0:
+                lower = index
+            neighbors = growbed[lower:index+3]
+#            print(neighbors)
+            repl = rules["".join(neighbors)]
+        except KeyError:
+            repl = '.'
+        newbed[index] = repl
+    growbed = newbed
+
+start_index = -len('...........')
+ans = 0
+for index,pot in enumerate(growbed):
+    if pot == '#':
+        ans += start_index + index
+print(ans)
