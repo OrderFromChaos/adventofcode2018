cbed676d7ca58c5c1ccabcd6a82f9d79152f7d7e
Syris Norelli <snore001@ucr.edu>
2018-12-29 18:12:09 -0800

    Removed incorrect jupyter usage %

--- 09/9.py
+++ 09/9.py
@@ -1,37 +1,10 @@
-### Taken from problem 5. Will use in a circular fashion
-class DoubleNode:
-    def __init__(self, value, prev, next):
-        self.value = value
-        self.prev = prev
-        self.next = next
-
-class DoubleList:
-    head = None
-    tail = None
-
-    def append(self, value):
-        if self.head is None: # If it's just one
-            self.head = self.tail = DoubleNode(value, None, None)
-        else: # If there's more than one
-            new_node = DoubleNode(value, self.tail, None)
-            self.tail.next = new_node
-            self.tail = new_node
-
-
-# Any time that head points at tail will cause an infinite regress, so we'll use lists, L.insert(i,x), and % instead
-#marbles = DoubleList()
-#marbles.append(0)
-#marbles.append(1)
-#marbles.tail.next = marbles.head # Tail is currently pointing at a head with .prev=None
-#marbles.head.prev = marbles.tail # Head is pointing at a tail with a head of .prev=None
-#marbles.tail.next = None # I think this causes infinite regress...
-
 def addMarble(marbles, marbleno, length, index):
     score = 0
     marbleno += 1
     if marbleno % 23 == 0:
-        print(marbles)
+#         print(marbles)
         score += marbleno
+        print(index)
         index -= 7
         score += marbles.pop(index)
         length -= 1
@@ -62,12 +35,12 @@ while score != endscore:
     if score != 0:
         #print(score)
         playerscore[currentplayer] += score
-    currentplayer += 1
+        currentplayer += 1
     if currentplayer == 10:
         currentplayer = 0
+    if score > endscore + 1000:
+        break
 print(playerscore)
 print(max(playerscore))
 
-#for i in range(25):
-#    marbles, marbleno, length, index, score = addMarble(marbles,marbleno,length,index)
 #    print(marbles,index,score)
